d26029a4e247b13b294864ef4103bc3e37b4948e
Umenyiora7 <117603473+Umenyiora7@users.noreply.github.com>
2022-12-26 10:27:08 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/prob30/strtoint.c
@@ -0,0 +1,21 @@
+#include"stdio.h"
+#include"stdlib.h"
+
+
+int strToInt(char*ch) {
+    if(strlen(ch)>0){
+    int number = 0;
+    int n=strlen(ch);
+    for(int i=0; i<n; i++)
+        number= number*10 + (*(ch+i)-'0');
+    return number;}else{printf("bad string");}
+}
+int main(){
+char ch[2]="8";
+int c;
+c=strToInt(ch);
+printf("%d",c);
+
+
+
+}
