0d5f171766dde4a2eae5f84973bcf03baefd2113
Umenyiora7 <117603473+Umenyiora7@users.noreply.github.com>
2022-12-26 13:09:22 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/prob12/reversbystaticndrecurs.c
@@ -0,0 +1,19 @@
+void revstr(char *str1)
+{
+static int i, len, temp;
+    len = strlen(str1); 
+    if (i < len/2) {
+        temp = str1[i];
+        str1[i] = str1[len - i - 1];
+        str1[len - i - 1] = temp;
+        i++;
+        revstr(str1); 
+  }}
+int main(){
+
+char ch[5];
+strcpy(ch,"hello");
+revstr(ch);
+printf("%s",ch);
+
+}
