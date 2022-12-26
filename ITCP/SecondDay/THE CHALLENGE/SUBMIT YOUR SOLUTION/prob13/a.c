89204324462da35633b857ac2fce5f6905e408cc
Umenyiora7 <117603473+Umenyiora7@users.noreply.github.com>
2022-12-26 13:26:34 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/prob13/a.c
@@ -0,0 +1,27 @@
+#include"stdio.h"
+#include"stdlib.h"
+
+
+/*int tst(char ch[30]) {
+int i;
+ for (i=0;i<strlen(ch);i++){
+if((ch[i]<65)||(ch[i]>90)){return 0;}
+}
+return 1;
+}*/
+int main(){
+char c[1];
+char ch[5]="hello",ch2[5]="olhel";
+int ex,k,i;
+ex=0;
+for(k=0;k<strlen(ch);k++){
+for(i=0;i<strlen(ch);i++){
+if(ch[i]==ch2[k]){
+ex=1;
+
+}}}
+if(ex==1){
+printf("true");}
+else{ printf("false");}
+
+}
