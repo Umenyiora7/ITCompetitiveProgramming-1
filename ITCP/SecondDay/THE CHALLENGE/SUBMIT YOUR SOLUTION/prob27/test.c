cdb68de49faf8a6100c0d52aaa51e2961b625e89
Umenyiora7 <117603473+Umenyiora7@users.noreply.github.com>
2022-12-26 10:36:09 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/prob27/test.c
@@ -0,0 +1,14 @@
+#include"stdio.h"
+#include"stdlib.h"
+
+
+int tst(int a,b) {
+   if((a+b)<100){return 1;}else{return 0;}
+}
+int main(){
+if(tst(5,6)){
+printf("true");}
+else{printf("false");  }
+
+
+}
