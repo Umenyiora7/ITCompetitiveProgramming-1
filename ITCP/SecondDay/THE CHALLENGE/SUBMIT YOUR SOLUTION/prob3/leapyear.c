fe01a241bb28526cc7b3e1ba6bdcf643e8c2ffbc
Umenyiora7 <117603473+Umenyiora7@users.noreply.github.com>
2022-12-26 10:20:34 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/prob3/leapyear.c
@@ -0,0 +1,21 @@
+#include"stdio.h"
+#include"stdlib.h"
+
+
+int leap(int y) {
+    if(y % 4==0){
+    return 1;}
+    else{
+    if(y%100 ==0){return 1;
+    }else{
+    if(y%400==0){return 1;}}
+    }return 0;
+}
+int main(){
+int y=2022;
+if(leap(y)){printf(" leap yr");}else{printf("not leap yr");}
+
+
+
+
+}
