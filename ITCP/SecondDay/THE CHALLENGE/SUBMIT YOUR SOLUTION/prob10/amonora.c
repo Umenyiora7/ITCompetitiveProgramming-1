74093d138eb886e86d967a0ef2bfb6775cd75895
Umenyiora7 <117603473+Umenyiora7@users.noreply.github.com>
2022-12-26 13:20:21 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/prob10/amonora.c
@@ -0,0 +1,40 @@
+int monotdc(int a[5]){
+int i;
+for(i=0;i<5;i++){
+if(a[i-1]<a[i])
+{
+
+return 0;
+}
+
+}
+return 1;
+}
+
+int monotc(int a[5])
+{
+int i;
+for(i=0;i<5;i++){
+if(a[i-1]>a[i])
+{
+
+return 0;
+}
+
+}
+return 1;
+}
+
+
+int main(){
+
+int a[5]={1,2,4,5};
+
+
+if(monotdc(a)||monotc(a)){
+printf("monotone");}
+else{
+printf("no monotone");
+
+}
+}
