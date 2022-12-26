a6ec30b8a0684cb12c643efe19d02e0bb493e856
Umenyiora7 <117603473+Umenyiora7@users.noreply.github.com>
2022-12-26 12:07:57 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/prob11/capitalltr.c
@@ -0,0 +1,20 @@
+#include"stdio.h"
+#include"stdlib.h"
+
+
+int tst(char ch[30]) {
+int i;
+ for (i=0;i<strlen(ch);i++){
+if((ch[i]<65)||(ch[i]>90)){return 0;}
+}
+return 1;
+}
+int main(){
+char ch[30];
+strcpy(ch,"Dz");
+if(tst(ch)){
+printf("true");}
+else{printf("false");  }
+
+
+}
